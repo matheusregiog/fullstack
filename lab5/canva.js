81f647d903523ee0eca45eb1e785452c63ce91f6
matheusregiog <matheus.regio.g@gmail.com>
2026-04-08 22:30:30 -0300

    Add files via upload

---
+++ lab5/canva.js
@@ -0,0 +1,205 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(150,50,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(100,100,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'brown';
+ctx.strokeStyle = 'brown';
+ctx.moveTo(60,150);
+ctx.lineTo(150,50);
+ctx.lineTo(250,150);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#8b4513';
+ctx.strokeStyle = '';
+ctx.moveTo(20,150);
+ctx.strokeRect(65,150,180,170);
+ctx.fillRect(65,150,180,170);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'aqua';
+ctx.strokeStyle = 'aqua';
+ctx.moveTo(20,150);
+ctx.strokeRect(85,180,50,50);
+ctx.fillRect(85,180,50,50);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'aqua';
+ctx.strokeStyle = 'aqua';
+ctx.moveTo(20,150);
+ctx.strokeRect(175,180,50,50);
+ctx.fillRect(175,180,50,50);
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'brown';
+ctx.strokeStyle = 'brown';
+ctx.moveTo(20,150);
+ctx.strokeRect(130,235,50,85);
+ctx.fillRect(130,235,50,85);
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'yellow';
+ctx.arc(300,75,45,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'gray';
+ctx.strokeStyle = 'gray';
+ctx.moveTo(20,150);
+ctx.strokeRect(1,322,500,170);
+ctx.fillRect(1,322,500,170);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'brown';
+ctx.strokeStyle = 'brown';
+ctx.moveTo(20,150);
+ctx.strokeRect(310,210,30,110);
+ctx.fillRect(310,210,30,110);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'green';
+ctx.strokeStyle = 'green';
+ctx.arc(325,200,30,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.fillStyle = 'black';
+ctx.strokeStyle = 'orange';
+ctx.arc(300,81,40,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(10,10,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(50,50,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(10,100,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(50,150,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(100,10,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'white';
+ctx.arc(200,10,5,3*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ctx.beginPath();
+// ctx.lineWidth = 2;
+// ctx.fillStyle = 'blue';
+// ctx.strokeStyle = 'red';
+// ctx.arc(200,200,50,1.5*Math.PI,2.5*Math.PI);
+// ctx.moveTo(200,200)
+// ctx.fill();
+// ctx.stroke();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.lineWidth = 2;
+// ctx.fillStyle = 'blue';
+// ctx.strokeStyle = 'red';
+// ctx.font = "90px Arial"
+// ctx.textAlign = "center";
+// ctx.fillText("Olá",200,350);
+// ctx.strokeText("Olá",200,350)
+// ctx.closePath();
+
